77f46ccb0c2a03af86c87fc1ddfb1ae560bbc962
Robert Ortiz <lastorbit1@gmail.com>
2026-09-19 23:37:42 +0200

    Added first function in script and some for future. Connected style and script to html doc. Added checkbox with removeTask function

---
+++ script.js
@@ -0,0 +1,27 @@
+const taskForm = document.getElementById("taskForm");
+
+// Date Section
+
+// function openDate(){}
+
+// function submitDate(){}
+
+// function getDate(){}
+
+// function removeDate(){}
+
+// function deleteDate(){}
+
+
+// Task Section 
+function openBox() {
+  taskForm.style.display = "flex";
+}
+
+// function submitTask(){}
+
+// function getTask(){}
+
+// function removeTask(){}
+
+// function deleteTask(){}
