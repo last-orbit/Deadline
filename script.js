2d5c5ce2e47cb0172cc2d7a0081dd5e4c317a087
Robert Ortiz <lastorbit1@gmail.com>
2026-09-23 00:23:05 +0200

    Finished completeTask function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const taskForm = document.getElementById("taskForm");
 const taskList = document.getElementById("taskList");
 const taskItem = document.getElementById("taskItem");
+let completeTaskList = document.getElementById("completeTaskList");
 
 /*
     Functions
@@ -33,6 +34,7 @@ function handleTaskSubmit(event) {
     taskName,
     status,
     priority,
+    complete: false,
   };
 
   setData(task);
@@ -43,11 +45,11 @@ function handleTaskSubmit(event) {
 
 function listTask() {
   taskList.innerHTML = ``;
-  const data = getData();
+  const data = getData("tasks");
   if (data) {
     taskList.innerHTML += `<ol>`;
     data.forEach((task, index) => {
-        let color;
+      let color;
       switch (task.priority) {
         case "1":
           color = "red";
@@ -55,32 +57,65 @@ function listTask() {
         case "2":
           color = "blue";
           break;
+        case "3":
+          color = "green";
+          break;
+        case "4":
+          color = "gray";
       }
-      taskList.innerHTML += `<li style="background-color:${color}">${task.taskName} <span><strong>status</strong>:${task.status}</span><span> priority :${task.priority}</span><div>
-      <input type="checkbox" id="checkbox" name="complete" />
-      <label for="checkbox" onclick="completeTask()">Complete</label>
-    </div></li>`;
+      taskList.innerHTML += `<div>
+                              <p style="background-color:${color}">${task.taskName}</p>
+                              <p>status</p>:${task.status} <p>priority :${task.priority}</p>
+                                <div>
+                                  <input type="checkbox" id="checkbox-${index}" name="complete" data-index=${index} />
+                                  <label for="checkbox-${index}">Complete</label>
+                                </div>
+                            </div>`;
     });
     taskList.innerHTML += `</ol>`;
   }
 }
 
-function getData() {
-  const data = localStorage.getItem("tasks");
+function listCompletedTasks() {
+  completeTaskList.innerHTML = ``;
+  const data = getData('completedTasks');
+  if (data) {
+    completeTaskList.innerHTML += `<ol>`;
+    data.forEach((task, index) => {
+      completeTaskList.innerHTML += `<div>
+                              <p>${task.taskName}</p>
+                              <p>status</p>:${task.status} <p>priority :${task.priority}</p>
+                            </div>`;
+    });
+    completeTaskList.innerHTML += `</ol>`;
+  }
+}
+
+function getData(key) {
+  const data = localStorage.getItem(key);
 
   return data ? JSON.parse(data) : [];
 }
 
 function setData(task) {
-  const tasks = getData();
-
-  tasks.push(task);
-
+  const tasks = getData("tasks");
+  tasks.unshift(task);
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 function completeTask(event) {
-  getData();
+  if (event.target.matches("input[type='checkbox'] ")) {
+    const index = event.target.dataset.index;
+    const tasks = getData("tasks");
+    const completedTasks = getData("completedTasks");
+    tasks[index].complete = event.target.checked;
+    completedTasks.unshift(tasks[index]);
+    tasks.splice(index, 1);
+    listTask()
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    localStorage.setItem("completedTasks", JSON.stringify(completedTasks));
+    listCompletedTasks();
+  }
 }
 
 // function deleteTask(){}
@@ -90,5 +125,7 @@ function completeTask(event) {
 */
 window.addEventListener("DOMContentLoaded", () => {
   listTask();
+  listCompletedTasks();
 });
 taskForm.addEventListener("submit", handleTaskSubmit);
+taskList.addEventListener("click", completeTask);
